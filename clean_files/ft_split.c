3ba5548ed34f9d665c806d97b774636d6ca621a2
Valeria Vagapova <lera.vagapova@gmail.com>
2022-11-12 23:45:58 +0200

    norm and clean ft_split

--- clean_files/ft_split.c
+++ clean_files/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: vvagapov <vvagapov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:36:02 by vvagapov          #+#    #+#             */
-/*   Updated: 2022/11/12 23:40:54 by vvagapov         ###   ########.fr       */
+/*   Updated: 2022/11/12 23:44:02 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,7 @@ static void	copy_word(char **res, const char *str, int i, int word_len)
 {
 	res[i][word_len] = '\0';
 	while (--word_len >= 0)
-	{
 		res[i][word_len] = str[word_len];
-		/* printf("copying %dth character of the word which is %c\n",
-            word_len, str[word_len]); */
-	}
-	//printf("resulting word: %s\n", res[i]);
 }
 
 char	**ft_split(char const *s, char c)
@@ -72,24 +67,20 @@ char	**ft_split(char const *s, char c)
 	if (!s)
 		return (NULL);
 	word_count = count_words(s, c);
-	//printf("word count is %d\n", word_count);
 	res = (char **)malloc(sizeof(char *) * (word_count + 1));
 	if (!res)
 		return (NULL);
 	i = 0;
 	while (i < word_count)
 	{
-		//printf("entering loop, i = %d\n", i);
 		while (*s && *s == c)
 			s++;
 		word_len = get_word_len(s, c);
 		res[i] = (char *)malloc(sizeof(char) * (word_len + 1));
 		if (!res[i])
 			return (handle_malloc_fail(res, i));
-		copy_word(res, s, i, word_len);
+		copy_word(res, s, i++, word_len);
 		s += word_len;
-		//printf("string after skipping the word: %s\n", s);
-		i++;
 	}
 	res[word_count] = NULL;
 	return (res);
