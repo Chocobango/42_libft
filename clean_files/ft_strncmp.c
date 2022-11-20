a74235fba7f721cfd23b40cbf64f8bb781b5a9c3
Valeria Vagapova <lera.vagapova@gmail.com>
2022-11-20 23:36:58 +0200

    fix ft_strncmp, config libft-unit-tests

--- clean_files/ft_strncmp.c
+++ clean_files/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vvagapov <vvagapov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 15:01:43 by vvagapov          #+#    #+#             */
-/*   Updated: 2022/11/12 17:38:42 by vvagapov         ###   ########.fr       */
+/*   Updated: 2022/11/20 23:21:04 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n < 1)
+		return (0);
 	i = 0;
 	while (i + 1 < n && s1[i] && s1[i] == s2[i])
 	{
@@ -23,36 +25,3 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-
-/* 
-void	test(const char *s1, const char *s2, size_t n)
-{
-	printf("'%s' and '%s', %lu chars: %d -> %d\n",
-			s1, s2, n, strncmp(s1, s2, n), ft_strncmp(s1, s2, n));
-}
-
-int	main(void)
-{
-	char	s1[2], s2[2];
-
-	test("Hello", "", 0);
-	test("", "", 0);
-	test("", "", 5);
-	test("a", "a", 1);
-	test("a", "b", 1);
-	test("123456", "123056", 2);
-	test("123456", "123056", 3);
-	test("123456", "123056", 4);
-	s1[0] = '\200';
-	s1[1] = '\0';
-	s2[0] = '\1';
-	s2[1] = '\0';
-	test(s1, s2, 1);
-	s2[0] = '\200';
-	s2[1] = '\0';
-	s2[0] = '\0';
-	s2[1] = '\0';
-	test(s1, s2, 1);
-	return(0);
-}
- */
