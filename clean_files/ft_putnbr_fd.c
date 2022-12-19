126e6c317008e9606372775ee24e67c5eccf952a
Valeria Vagapova <lera.vagapova@gmail.com>
2022-12-19 21:36:07 +0200

    fix putnbr

--- clean_files/ft_putnbr_fd.c
+++ clean_files/ft_putnbr_fd.c
@@ -6,26 +6,31 @@
 /*   By: vvagapov <vvagapov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 19:31:08 by vvagapov          #+#    #+#             */
-/*   Updated: 2022/12/19 19:09:24 by vvagapov         ###   ########.fr       */
+/*   Updated: 2022/12/19 21:34:09 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (!n)
-		ft_putchar_fd('0', fd);
-	if (n == -2147483648)
-	{
-		ft_putstr_fd("-2147483648", fd);
-		return;
-	}
 	if (n < 0)
 	{
+		if (n == -2147483648)
+		{
+			ft_putstr_fd("-2147483648", fd);
+			return ;
+		}
 		ft_putchar_fd('-', fd);
 		n *= -1;
 	}
-	// TODO: number part
+	if (n < 10)
+	{
+		ft_putchar_fd('0' + n, fd);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd('0' + n % 10, fd);
+	}
 }
